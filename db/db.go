92add08ef06b4d4e24e06ceb984283f50c940588
hojjatAali <mahyaraali@gmail.com.com>
2025-01-14 13:41:09 +0330

    feat: Add config

--- db/db.go
+++ db/db.go
@@ -1,9 +1,11 @@
 package db
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"user_wallet/config"
 	"user_wallet/struct"
 )
 
@@ -11,16 +13,24 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
-	dsn := "host=localhost user=postgres password=Martin1992& dbname=go_test sslmode=disable"
+
+	config.LoadConfig()
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.AppConfig.Database.Host,
+		config.AppConfig.Database.User,
+		config.AppConfig.Database.Password,
+		config.AppConfig.Database.DbName,
+		config.AppConfig.Database.SSLMode,
+	)
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect to database:", err)
 	}
-
 	err = DB.AutoMigrate(&structs.User{}, &structs.Wallet{})
 	if err != nil {
 		log.Fatal("failed to migrate database:", err)
 	}
-
 	log.Println("Connected to the database successfully and schema migrated!")
 }
